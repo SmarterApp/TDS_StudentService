30adaaca73234535b7fb3192fe8c4efce4fb4096
Todd Miller <tmiller@fairwaytech.com>
2016-09-13 12:32:28 -0700

    Added Web integration tests

---
+++ service/src/test/java/tds/student/web/endpoints/HealthControllerIntegrationTests.java
@@ -0,0 +1,26 @@
+package tds.student.web.endpoints;
+
+
+import com.jayway.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static com.jayway.restassured.RestAssured.given;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest (webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class HealthControllerIntegrationTests {
+    @Test
+    public void shouldReturnOk() {
+        given()
+            .accept(ContentType.JSON)
+            .when()
+        .get("/student/isAlive")
+        .then()
+            .contentType(ContentType.JSON)
+            .statusCode(200);
+    }
+}
+
---
+++ service/src/test/java/tds/student/web/endpoints/StudentControllerIntegrationTests.java
@@ -0,0 +1,39 @@
+package tds.student.web.endpoints;
+
+import com.jayway.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class StudentControllerIntegrationTests {
+    @Test
+    public void shouldReturnSession() {
+        given()
+            .accept(ContentType.JSON)
+            .when()
+            .get(String.format("/student/%d", 1))
+            .then()
+            .contentType(ContentType.JSON)
+            .statusCode(200)
+            .body("student.id", equalTo(1))
+            .body("student.studentId", equalTo("adv001"))
+            .body("student.stateCode", equalTo("CA"))
+            .body("student.clientName", equalTo("SBAC_PT"));
+    }
+
+    @Test
+    public void shouldReturnNotFound() {
+        given()
+            .accept(ContentType.JSON)
+            .when()
+            .get(String.format("/student/%s", 999999))
+            .then()
+            .statusCode(404);
+    }
+}
