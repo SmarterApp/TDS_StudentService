254bb23177c5d5495c10178b28204ef535e2a3e3
Todd Miller <tmiller@fairwaytech.com>
2016-09-13 13:46:49 -0700

    Revert to using the deprecated rest classes to work well with rest assured

--- service/src/test/java/tds/student/web/endpoints/HealthControllerIntegrationTests.java
+++ service/src/test/java/tds/student/web/endpoints/HealthControllerIntegrationTests.java
@@ -4,13 +4,20 @@ package tds.student.web.endpoints;
 import com.jayway.restassured.http.ContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import tds.student.StudentServiceApplication;
 
 import static com.jayway.restassured.RestAssured.given;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest (webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringApplicationConfiguration(classes = StudentServiceApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:8080")
 public class HealthControllerIntegrationTests {
     @Test
     public void shouldReturnOk() {
--- service/src/test/java/tds/student/web/endpoints/StudentControllerIntegrationTests.java
+++ service/src/test/java/tds/student/web/endpoints/StudentControllerIntegrationTests.java
@@ -3,14 +3,20 @@ package tds.student.web.endpoints;
 import com.jayway.restassured.http.ContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import tds.student.StudentServiceApplication;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringApplicationConfiguration(classes = StudentServiceApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:8080")
 public class StudentControllerIntegrationTests {
     @Test
     public void shouldReturnStudent() {
