978691db59053eba596fc03b902df49b1b077e23
Todd Miller <tmiller@fairwaytech.com>
2016-10-04 17:29:42 -0700

    Add the IRtsPackageReader to the spring configuration

--- service/src/main/java/tds/student/StudentServiceApplication.java
+++ service/src/main/java/tds/student/StudentServiceApplication.java
@@ -2,10 +2,19 @@ package tds.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import tds.student.legacy.rtspackage.IRtsPackageReader;
+import tds.student.legacy.rtspackage.student.StudentPackageReader;
 
 @SpringBootApplication
 public class StudentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentServiceApplication.class, args);
+    }
+
+    @Bean
+    public IRtsPackageReader getIRtsPackageReader() {
+        return new StudentPackageReader();
     }
 }
