d27d1cd0c4f4ff3a77adbcb91271bd076078751a
Todd Miller <tmiller@fairwaytech.com>
2016-10-23 11:26:52 -0700

    Broke out configuration settings into configuration classes

--- service/src/main/java/tds/student/StudentServiceApplication.java
+++ service/src/main/java/tds/student/StudentServiceApplication.java
@@ -2,19 +2,10 @@ package tds.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import tds.dll.common.rtspackage.IRtsPackageReader;
-import tds.dll.common.rtspackage.student.StudentPackageReader;
 
 @SpringBootApplication
 public class StudentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentServiceApplication.class, args);
-    }
-
-    @Bean
-    public IRtsPackageReader getIRtsPackageReader() {
-        return new StudentPackageReader();
     }
 }
---
+++ service/src/main/java/tds/student/configuration/WebConfiguration.java
@@ -0,0 +1,18 @@
+package tds.student.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.util.UrlPathHelper;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurationSupport {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
+
+}
