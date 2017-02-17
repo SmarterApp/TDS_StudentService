d22b6f2c87b105bdba52c54065133b938f073cf2
wtritch <will@gearlaunch.com>
2017-02-17 10:13:23 -0800

    Externalize configuration properties

--- service/src/main/docker/docker-startup.sh
+++ service/src/main/docker/docker-startup.sh
@@ -6,19 +6,4 @@
 #
 #-----------------------------------------------------------------------------------------------------------------------
 
-java \
-    -Dspring.datasource.url="jdbc:mysql://${STUDENT_DB_HOST}:${STUDENT_DB_PORT}/${STUDENT_DB_NAME}" \
-    -Dspring.datasource.username="${STUDENT_DB_USER}" \
-    -Dspring.datasource.password="${STUDENT_DB_PASSWORD}" \
-    -Dspring.datasource.type=com.zaxxer.hikari.HikariDataSource \
-    -Dspring.datasource.driver-class-name=com.mysql.jdbc.Driver \
-    -Dspring.datasource.testWhileIdle=true \
-    -Dspring.datasource.timeBetweenEvictionRunsMillis=60000 \
-    -Dspring.datasource.validationQuery="SELECT 1" \
-    -jar /tds-student-service.jar \
-    --server-port="8080" \
-    --server.undertow.buffer-size=16384 \
-    --server.undertow.buffers-per-region=20 \
-    --server.undertow.io-threads=64 \
-    --server.undertow.worker-threads=512 \
-    --server.undertow.direct-buffers=true \
+java -jar /tds-student-service.jar
